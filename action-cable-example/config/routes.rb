95a9e6ed87b3f7718a80176c2d8a1eaf9604f29d
Benjamin Schwartz <bs4308@nyu.edu>
2022-11-20 13:41:53 -0500

    Run migrations

--- action-cable-example/config/routes.rb
+++ action-cable-example/config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "welcome#about"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/signup', to: "registrations#new"
+  post '/signup', to: "registrations#create"
+
+  get 'login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+
+  get 'users/:id', to: "users#show", as: "profile"
+
+  resources :chatrooms, param: :slug
+  resources :messages
+
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
 end
--- action-cable-example/db/migrate/20221120183234_create_chatrooms.rb
+++ action-cable-example/db/migrate/20221120183234_create_chatrooms.rb
@@ -1,7 +1,7 @@
 class CreateChatrooms < ActiveRecord::Migration[7.0]
   def change
     create_table :chatrooms do |t|
-
+      t.string :topic
       t.timestamps
     end
   end
